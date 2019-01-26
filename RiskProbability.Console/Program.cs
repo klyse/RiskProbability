5dad6dc3c8cd0bfba3e300ad1f73dc94550cd6c2
Unknown <Klaus.Pruenster@gmail.com>
2019-01-26 21:17:51 +0100

    added console program

---
+++ RiskProbability.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using RiskProbability.Application;
+using static System.Int32;
+
+namespace RiskProbability.Console
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			System.Console.WriteLine("Hello World!");
+			System.Console.Write("Attacker count: ");
+			var attacker = System.Console.ReadLine();
+
+			if (!TryParse(attacker, out var attackerCount))
+			{
+				System.Console.WriteLine("Attacker count input format is wrong.");
+				return;
+			}
+
+			System.Console.Write("Defender count: ");
+
+			var defender = System.Console.ReadLine();
+
+
+			if (!TryParse(defender, out var defenderCount))
+			{
+				System.Console.WriteLine("Defender count input format is wrong.");
+				return;
+			}
+
+			System.Console.WriteLine("The Attacker has a " + Probability.GetProbabilityString(attackerCount, defenderCount) + " chance to win.");
+
+			System.Console.ReadLine();
+		}
+	}
+}
